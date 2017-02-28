37179c1d3800c92fce45d26a2b0a8b12a44a7e05
Yves Lang <yves.lang@abilita.ch>
2017-02-28 07:21:30 +0000

    Save Working / Time Format wrong

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -22,8 +22,29 @@ sap.ui.define([], function() {
 				return "";
 			}
 
-		//		return parseFloat(sValue).toFixed(0);
-		return oNumberFormat.format(sValue);                                                
+			//		return parseFloat(sValue).toFixed(0);
+			return oNumberFormat.format(sValue);
+		},
+		time: function(value) {
+			//                             console.log(value);                        
+			if (value) {
+
+				var date = new Date(value.ms);
+				//                             console.log("date po value.ms: "+ date);
+				var timeinmiliseconds = date.getTime(); //date.getTime(); //date.getSeconds(); //date.getTime();
+				//                             console.log(timeinmiliseconds);
+				var oTimeFormat = sap.ui.core.format.DateFormat.getTimeInstance({
+					pattern: "KK:mm:ss a"
+				});
+				var TZOffsetMs = new Date(0).getTimezoneOffset() * 60 * 1000;
+				//                             console.log(TZOffsetMs);
+				var timeStr = oTimeFormat.format(new Date(timeinmiliseconds + TZOffsetMs));
+				//                             console.log(timeStr);
+
+				return timeStr;
+			} else {
+				return value;
+			}
 		}
 	};
 
