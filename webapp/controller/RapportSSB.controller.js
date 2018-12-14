1fbb79b2038e3b1c9a5164673054442f3f2ca4f4
Yves Lang <yves.lang@abilita.ch>
2018-12-14 15:47:14 +0000

    Start und Ende Zeit / Datum dürfen nicht identisch sein. So wird verhindert das rapporte intialzeiten ( 00:00 ) erfasst werden könnnen

--- webapp/controller/RapportSSB.controller.js
+++ webapp/controller/RapportSSB.controller.js
@@ -336,6 +336,15 @@ sap.ui.define([
 						//actions: [sap.m.MessageBox.Action.YES, sap.m.MessageBox.Action.NO],
 						//onClose: function(oAction) { error = true; } 
 				});
+			} else if (oContext.getProperty("Datum").toDateString() === oContext.getProperty("DatumTo").toDateString() && oContext.getProperty(
+					"Zeit/ms") === oContext.getProperty(
+					"ZeitTo/ms")) {
+				sap.m.MessageBox.show("Die Start und Endzeit darf nicht identisch sein!", {
+					icon: sap.m.MessageBox.Icon.ERROR,
+					title: "Fehler" //,
+						//actions: [sap.m.MessageBox.Action.YES, sap.m.MessageBox.Action.NO],
+						//onClose: function(oAction) { error = true; } 
+				});
 			} else {
 				// if (!oContext.getProperty("Signatur") // Muss unterschrieben sein 
 				// 	&& !this.getModel("rapportSSBView").getProperty("/poweruser") // oder Poweruser 
